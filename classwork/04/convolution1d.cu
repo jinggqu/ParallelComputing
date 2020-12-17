8aa37a0e4ba6a8205b2a557973e482eec410cc9a
sudrizzz <sudrizzz@gmail.com>
2020-12-17 13:49:40 +0800

    update code style

--- classwork/04/convolution1d.cu
+++ classwork/04/convolution1d.cu
@@ -4,58 +4,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void convolution_1D_basic_kernel(float* N, float* M, float* P, int MASK_WIDTH, int Width) {
+__global__ void convolution_1D_basic_kernel(float* in, float* mask, float* out, int mask_size, int input_size) {
 
+    float val = 0;
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    float Pvalue = 0;
-    int n_start_point = i - (MASK_WIDTH / 2);
+    int n_start_point = i - (mask_size / 2);
 
-    if (i < Width) {
-        for (int j = 0; j < MASK_WIDTH; j++) {
-            if ((n_start_point + j) >= 0 && (n_start_point + j < Width)) {
-                Pvalue += N[n_start_point + j] * M[j];
+    if (i < input_size) {
+        for (int j = 0; j < mask_size; j++) {
+            if ((n_start_point + j) >= 0 && (n_start_point + j < input_size)) {
+                val += in[n_start_point + j] * mask[j];
             }
         }
-        P[i] = Pvalue;
+        out[i] = val;
     }
 }
 
 
 int main() {
-    int nSize = 7, mSize = 5;
-    float* host_N, * host_P, * device_N, * device_M, * device_P;
-    float host_M[5] = { 3, 4, 5, 4, 3 };
+    int input_size = 7, mask_size = 5;
+    float* host_in, * host_out, * device_in, * device_mask, * device_out;
+    float host_mask[5] = { 3, 4, 5, 4, 3 };
 
-    host_N = (float*)malloc(nSize * sizeof(float));
-    host_P = (float*)malloc(nSize * sizeof(float));
-    for (int i = 0; i < nSize; i++) {
-        host_N[i] = i * 1.0 + 1;
+    host_in = (float*)malloc(input_size * sizeof(float));
+    host_out = (float*)malloc(input_size * sizeof(float));
+    for (int i = 0; i < input_size; i++) {
+        host_in[i] = i + 1;
     }
 
-    cudaMalloc((void**)&device_N, nSize * sizeof(float));
-    cudaMalloc((void**)&device_M, mSize * sizeof(float));
-    cudaMalloc((void**)&device_P, nSize * sizeof(float));
-    cudaMemcpy(device_N, host_N, nSize * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_M, host_M, mSize * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&device_in, input_size * sizeof(float));
+    cudaMalloc((void**)&device_mask, mask_size * sizeof(float));
+    cudaMalloc((void**)&device_out, input_size * sizeof(float));
+    cudaMemcpy(device_in, host_in, input_size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_mask, host_mask, mask_size * sizeof(float), cudaMemcpyHostToDevice);
 
-    dim3 threadsPerBlock(nSize);
+    dim3 threadsPerBlock(input_size);
     dim3 blocksPerGrid(1);
 
-    convolution_1D_basic_kernel <<< blocksPerGrid, threadsPerBlock >>> (device_N, device_M, device_P, mSize, nSize);
+    convolution_1D_basic_kernel <<< blocksPerGrid, threadsPerBlock >>> (device_in, device_mask, device_out, mask_size, input_size);
     cudaDeviceSynchronize();
 
-    cudaMemcpy(host_P, device_P, nSize * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_out, device_out, input_size * sizeof(float), cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i < nSize; ++i) {
-        printf("%.f\t", host_P[i]);
+    for (int i = 0; i < input_size; ++i) {
+        printf("%.f\t", host_out[i]);
     }
     printf("\n");
 
-    cudaFree(device_N);
-    cudaFree(device_M);
-    cudaFree(device_P);
-    free(host_N);
-    free(host_P);
+    cudaFree(device_in);
+    cudaFree(device_mask);
+    cudaFree(device_out);
+    free(host_in);
+    free(host_out);
     return 0;
 }
 
