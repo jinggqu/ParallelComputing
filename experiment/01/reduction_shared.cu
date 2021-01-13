4e622b84f0df44ab2ba082a6e2bf24899e066d8f
sudrizzz <sudrizzz@gmail.com>
2021-01-13 13:57:15 +0800

    update code

--- experiment/01/reduction_shared.cu
+++ experiment/01/reduction_shared.cu
@@ -14,8 +14,7 @@ __global__ void getMaxValueOfRow(float *d_arr, float *maxArray) {
     ds_arr[t] = d_arr[t + bid * N];
     for (unsigned int stride = 1; stride < blockDim.x;  stride *= 2) {
         __syncthreads();
-        if (t % (2 * stride) == 0)
-            ds_arr[t] = ds_arr[t + stride] > ds_arr[t] ? ds_arr[t + stride] : ds_arr[t];
+        ds_arr[t] = ds_arr[t + stride] > ds_arr[t] ? ds_arr[t + stride] : ds_arr[t];
     }
     maxArray[bid % N] = ds_arr[t];
 }
